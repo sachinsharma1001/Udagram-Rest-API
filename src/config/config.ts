2c82fdc2c3b9453dc8f7d74e4cac4b4550d58aa0
sachinsharma1001 <sachin.sharma100197@gmail.com>
2020-10-08 23:32:31 +0530

    Adding files to udagram-rest-api project

---
+++ src/config/config.ts
@@ -0,0 +1,19 @@
+export const config = {
+  "dev": {
+    "username": "udagramsachindev",
+    "password": "udagramsachindev",
+    "database": "udagramsachindev",
+    "host": "udagramsachindev.cp1awku8suyh.us-east-2.rds.amazonaws.com",
+    "dialect": "postgres",
+    "aws_region": "us-east-2",
+    "aws_profile": "default",
+    "aws_media_bucket": "udagram-sachin-dev"
+  },
+  "prod": {
+    "username": "",
+    "password": "",
+    "database": "udagram_prod",
+    "host": "",
+    "dialect": "postgres"
+  }
+}
