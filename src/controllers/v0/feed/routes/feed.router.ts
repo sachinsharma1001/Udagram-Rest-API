2ea764fd296a0f7ebbaecca1daaec5d96c4bc2bd
sachinsharma1001 <sachin.sharma100197@gmail.com>
2020-10-26 23:37:08 +0530

    Adding environment variables in the udagram api

--- src/controllers/v0/feed/routes/feed.router.ts
+++ src/controllers/v0/feed/routes/feed.router.ts
@@ -9,14 +9,13 @@ const router: Router = Router();
 router.get('/', async (req: Request, res: Response) => {
     const items = await FeedItem.findAndCountAll({order: [['id', 'DESC']]});
     items.rows.map((item) => {
-            if(item.url) {
-                item.url = AWS.getGetSignedUrl(item.url);
-            }
+        if(item.url) {
+            item.url = AWS.getGetSignedUrl(item.url);
+        }
     });
     res.send(items);
 });
 
-//@TODO
 //Add an endpoint to GET a specific resource by Primary Key
 router.get('/:id', async (req: Request, res: Response) => {
     let { id } = req.params; 
@@ -28,16 +27,13 @@ router.get('/:id', async (req: Request, res: Response) => {
 })
 
 // update a specific resource
-router.patch('/:id', requireAuth, async (req: Request, res: Response) => {
-        //@TODO try it yourself
-        res.send(500).send("not implemented")
+router.patch('/:id', requireAuth, async (req: Request, res: Response) => {   
+    res.send(500).send("not implemented")
 });
 
 
 // Get a signed url to put a new item in the bucket
-router.get('/signed-url/:fileName', 
-    requireAuth, 
-    async (req: Request, res: Response) => {
+router.get('/signed-url/:fileName', requireAuth, async (req: Request, res: Response) => {
     let { fileName } = req.params;
     const url = AWS.getPutSignedUrl(fileName);
     res.status(201).send({url: url});
